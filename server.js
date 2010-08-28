ff6a5c6682618120aef84a8759f932f936f44e64
James Carr <james.r.carr@gmail.com>
2010-08-27 20:50:08 -0400

    trying to get settled for joyent

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index.jade', {
     locals: {
-      title: 'Express'
+      title: 'Team Done.js'
     }
   });
 });
