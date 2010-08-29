2a50557b6f7c651faa9d330f7934ba9be20a6c0f
James Carr <james.r.carr@gmail.com>
2010-08-28 23:06:04 -0400

    isolate app creation

--- server.js
+++ server.js
@@ -10,35 +10,31 @@ var express = require('express'),
   routes = require('routes')
   config = require('config');
 
-var app = express.createServer();
-// Configuration
+var app = (function(){
+  var app = express.createServer();
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.use(connect.bodyDecoder());
+    app.use(connect.methodOverride());
+    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(app.router);
+    app.use(connect.staticProvider(__dirname + '/public'));
+  });
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.use(connect.bodyDecoder());
-  app.use(connect.methodOverride());
-  app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
-  app.use(app.router);
-  app.use(connect.staticProvider(__dirname + '/public'));
-});
+  app.configure('development', function(){
+    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+  });
 
-app.configure('development', function(){
-  app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(connect.errorHandler()); 
-});
-
+  app.configure('production', function(){
+    app.use(connect.errorHandler()); 
+  });
+  return app;
+}();
 // Custom config management
 var settings = config.load(app.settings['env']);
 
 // Routes
-routes.wire(app, settings)
-
-
-
-
+routes.wire(app, settings);
 
 app.listen(parseInt(process.env.PORT) || 3000, null, function(){
   console.log("Server started on port 3000")
